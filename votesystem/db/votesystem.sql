7e2cf62d8ae0bffab5c3fa615dcb9f9e3d1a7796
IT21002588 <87350808+IT21002588@users.noreply.github.com>
2023-05-28 15:46:14 +0530

    Update votesystem.sql

--- votesystem/db/votesystem.sql
+++ votesystem/db/votesystem.sql
@@ -1,11 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 5.0.2
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: May 27, 2021 at 02:28 PM
--- Server version: 10.4.14-MariaDB
--- PHP Version: 7.4.9
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
